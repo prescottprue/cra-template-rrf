5cfa67d52c6fd287d3ac3346abf0c711e54f2702
Harish Rana <r.shivi@yahoo.com>
2020-01-08 18:06:57 +0530

    chore(development): Add capability to run project from the directory
    Make changes package.json script to copy the working project to the template directory
    
    refactor(template): remove code from template directory
    
    fix(scripts): change the copy script to make config file

---
+++ src/configDummy.js
@@ -0,0 +1,32 @@
+// export const fbConfig = {
+//   apiKey: 'your-project api key',
+//   authDomain: 'your-project.firebaseapp.com',
+//   databaseURL: 'https://your-project.firebaseio.com',
+//   storageBucket: 'your-project.appspot.com',
+//   messagingSenderId: '823357791673',
+//   projectId: 'your-project',
+// };
+
+export const rfConfig = {
+  userProfile: 'users', // root that user profiles are written to
+  useFirestoreForProfile: true, // Save profile to Firestore instead of Real Time Database
+  useFirestoreForStorageMeta: true, // Metadata associated with storage file uploads goes to Firestore
+};
+
+export const fbConfig = {
+  apiKey: ' AIzaSyA0d1K8ycSumChLt83L0l2dAzNEsEXETAM ',
+  authDomain: 'reduxfirebaseexample-cc8f4.firebaseapp.com',
+  databaseURL: 'https://reduxfirebaseexample-cc8f4.firebaseio.com',
+  projectId: 'reduxfirebaseexample-cc8f4',
+  storageBucket: 'reduxfirebaseexample-cc8f4.appspot.com',
+  messagingSenderId: '791977720751',
+  measurementId: '791977720751',
+  appId: '1:791977720751:web:e7d8c463fde349e1150d5a',
+};
+
+export const segmentId = '';
+
+export const publicVapidKey =
+  'BNFE71Zyl2H66TCrLz8RdWJKipJBEiw5TsJIPwm4gc0pCcucfRj1tSwHPRJad4c85fJraDYubWRVrQo_8D2PLzo';
+
+export const sentryDsn = '';
